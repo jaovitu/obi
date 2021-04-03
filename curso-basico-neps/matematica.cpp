40506045c81f9fc263e386f2e5cd48eeceb409a8
JvGomess <vg913193@gmail.com>
2021-04-02 21:38:39 -0300

    matematica

---
+++ curso-basico-neps/matematica.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    cout << pow(2, 10) << endl;
+    cout << sqrt(36) << endl;
+
+    cout << floor(1.8) << endl;
+    cout << ceil(1.1) << endl;
+
+    return 0;
+}
